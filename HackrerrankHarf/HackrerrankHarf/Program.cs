9420417a3c3ccbdc9a458b6140204b99769037e3
lprtan <95475477+lprtan@users.noreply.github.com>
2022-10-07 12:20:43 +0300

    Add files via upload

---
+++ HackrerrankHarf/HackrerrankHarf/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackrerrankHarf
+{
+    class Program
+    {
+        public static string function(string cumle)
+        {
+            string mesaj = "";
+            List<string> list = new List<string>();
+            var kelime = cumle.Split(' ');
+
+            for (int i =kelime.Length-1; i >=0; i--)
+            {
+                char[] charsArray = kelime[i].ToCharArray();
+
+                for (int j = 0; j < charsArray.Length; j++)
+                {
+                    if (charsArray[j]>='A' && charsArray[j]<='Z')
+                    {
+                        mesaj += charsArray[j].ToString().ToLower();
+                    }
+                    else if (charsArray[j] >= 'a' && charsArray[j] <= 'z')
+                    {
+                        mesaj += charsArray[j].ToString().ToUpper();
+                    }
+                }
+                mesaj += " ";
+            }
+
+            return mesaj;
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(function("RuN dOg"));
+        }
+    }
+}
