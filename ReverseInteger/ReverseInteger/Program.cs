35898f5eca6e36f543468863dfaff9c6541c2909
Alperen Tan <95475477+lprtan@users.noreply.github.com>
2023-05-16 13:51:53 +0300

    Add files via upload

---
+++ ReverseInteger/ReverseInteger/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseInteger
+{
+    // Soru Link: https://leetcode.com/problems/reverse-integer/description/
+    class Program
+    {
+        public static int Reverse(int x)
+        {
+            string num = x.ToString();
+            string result = "";
+            List<string> list = num.Select(c => c.ToString()).ToList();
+
+            if (x < 0)
+            {
+                list.Reverse();
+                list.RemoveAt(list.Count - 1);
+                if (x % 10 == 0)
+                {
+                    list.RemoveAt(0);
+                    result = "-"+String.Join("", list);
+                    if (int.TryParse(result, out int parsedResult))
+                    {
+                        return parsedResult;
+                    }
+                }
+                result = "-" + String.Join("", list);
+                if (int.TryParse(result, out int parsedResult2))
+                {
+                    return parsedResult2;
+                }
+            }
+            else if (x >= 0)
+            {
+                list.Reverse();
+                if (x % 10 == 0 && x!=0)
+                {
+                    list.RemoveAt(0);
+                    result = String.Join("", list);
+                    if (int.TryParse(result, out int parsedResult3))
+                    {
+                        return parsedResult3;
+                    }
+                }
+                result = String.Join("", list);
+                if (int.TryParse(result, out int parsedResult4))
+                {
+                    return parsedResult4;
+                }
+            }
+
+            return 0;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse(0));
+        }
+    }
+}
