dc87844985fa23ae0fe3ca5335f97fe99fee2d15
Alperen Tan <95475477+lprtan@users.noreply.github.com>
2023-11-09 16:06:03 +0300

    Add files via upload

---
+++ RemoveTrailingZerosFromaString/RemoveTrailingZerosFromaString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace RemoveTrailingZerosFromaString
+{
+    class Program
+    {
+        public static string RemoveTrailingZeros(string num)
+        {
+            BigInteger number = BigInteger.Parse(num);
+            const int divisive = 10;
+            bool status = true;
+
+            while (status)
+            {
+                status = number % divisive == 0;
+                if (status)
+                {
+                    number = number / divisive;
+                }
+            }
+
+            return number.ToString();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveTrailingZeros("1720865079269529096765717822459"));
+        }
+    }
+}
