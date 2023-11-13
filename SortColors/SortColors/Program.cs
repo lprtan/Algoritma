b7022fec063f9f1e3843888a4a10e38a7fd66d33
Alperen Tan <95475477+lprtan@users.noreply.github.com>
2023-11-13 16:08:40 +0300

    Add files via upload

---
+++ SortColors/SortColors/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SortColors
+{
+    class Program
+    {
+        // Question link: https://leetcode.com/problems/sort-colors/description/
+        public static void SortColors(int[] nums)
+        {
+            Array.Sort(nums);
+            foreach (int item in nums)
+            {
+                Console.Write(item);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 0, 2, 1, 1, 0 };
+            SortColors(arr);
+        }
+    }
+}
