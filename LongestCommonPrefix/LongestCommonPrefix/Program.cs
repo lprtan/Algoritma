4ef4cc5e7dc83a7397298c6aae2668748d559723
Alperen Tan <95475477+lprtan@users.noreply.github.com>
2024-02-19 16:54:07 +0300

    Add files via upload

---
+++ LongestCommonPrefix/LongestCommonPrefix/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LongestCommonPrefix
+{
+    class Program
+    {
+        //Question Url: https://leetcode.com/problems/longest-common-prefix/submissions/1179895328/
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            int m = 0;
+            int count = 0;
+            string result = "";
+            while (m < strs[0].Length)
+            {
+                char x = strs[0][m];
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (m >= strs[j].Length || strs[j][m] != x)
+                    {
+                        return result;
+                    }
+                }
+                result += x;
+                m++;
+            }
+
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] strs = { "flower", "flow", "flight" };
+            Console.WriteLine(LongestCommonPrefix(strs));
+        }
+    }
+}
